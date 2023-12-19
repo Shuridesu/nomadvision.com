2c9ab7601255c3efe67d547ced795c319f5a6be2
shuri <shurikikuti@icloud.com>
2023-12-20 03:19:26 +1100

    delete again

--- nomad-front/lib/getPost.tsx
+++
@@ -1,6 +0,0 @@
-export default async function getPost(postSlug:string) {
-    const res = await fetch(`https://nomadvision-e83b637ecb0f.herokuapp.com/posts/${postSlug}`,{
-      next:{revalidate:10}})
-    if(!res.ok)return undefined
-  return res.json()
-}
