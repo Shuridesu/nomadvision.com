1c6c87cfeb4955f2de177f0d2bd39db2a275ca6e
shuri <shurikikuti@icloud.com>
2023-12-20 15:51:56 +1100

    modify

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -6,7 +6,7 @@ export default function Hero() {
   return (
     <Fade triggerOnce>
       <div className="relative w-full h-screen bg-[url('/a5.png')] bg-cover bg-right lg:bg-center">
-        <span className="max-w-[600px] sm:max-w-[850px] absolute top-48 right-1/3  text-4xl font-bold text-black ms-10 md:text-6xl text-left bg-gray-200 bg-opacity-40 lg:bg-opacity-0 py-3 px-3">
+        <span className="max-w-[600px] sm:max-w-[850px] absolute top-48 right-1/3  text-4xl font-bold text-black ms-10 md:text-6xl text-left bg-gray-200 bg-opacity-40 lg:bg-opacity-0 py-3 px-3 translate-x-14">
           <h1>
             WHERE <span className="text-blue-900">AI,DATA</span> FOR BUSINESS
             AND <span className="text-blue-900">NETWORKING COMMUNITY</span>{" "}
