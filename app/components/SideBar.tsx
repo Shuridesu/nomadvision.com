c8390304204c11942679ec7dd8a56a61ad25bfe1
shuri <shurikikuti@icloud.com>
2023-12-20 16:31:38 +1100

    modify sidebar

--- app/components/SideBar.tsx
+++ app/components/SideBar.tsx
@@ -74,7 +74,7 @@ export default function SideBar() {
       </button>
       <div
         className={`absolute right-0 h-max pb-14 rounded-xl transform transition-transform duration-500 ease-in-out ${
-          isOpen ? "translate-y-0" : "translate-x-full"
+          isOpen ? "translate-y-0" : "translate-x-full hidden"
         } w-2/3 sm:w-1/3 bg-blue-500 bg-opacity-80 shadow-md z-1 lg:hidden`}
       >
         <div>
