160c72a0bea429ca707ec20d85e3f23de7d0ec4e
shuri <shurikikuti@icloud.com>
2024-01-07 18:48:35 +1100

    temprary save

---
+++ app/loading.tsx
@@ -0,0 +1,14 @@
+
+const Loading = () =>{
+    return(
+    <div className="flex justify-center items-center h-screen">
+        <div className="loading loading-bars loading-lg"></div>
+    </div>
+    )
+}
+
+export default Loading;
+
+
+
+
