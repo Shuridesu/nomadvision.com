aa71b0b6216384f492b2ce29b3a0e7abc6174efc
shuri <shurikikuti@icloud.com>
2024-01-20 15:36:10 +1100

    edit ai-software api again

--- lib/getAiSoftwarePosts.tsx
+++ lib/getAiSoftwarePosts.tsx
@@ -1,9 +1,25 @@
 import React from 'react'
 
-export default async function getAiSoftwarePosts() {
-    const data = await fetch('https://nomadvision-e83b637ecb0f.herokuapp.com/posts/ai-software/', 
-      { next: { revalidate: 10 } })
-    if (!data.ok) return undefined
-  return data.json()
+const fetchAPI = async (url: string, options: RequestInit) => {
+  const apiUrl = process.env.API_URL
 
+  if (!apiUrl) {
+    throw new Error("API URLが設定されていません")
+  }
+
+  const response = await fetch(`${apiUrl}${url}`, options)
+
+  if (!response.ok) {
+    throw new Error("APIでエラーが発生しました")
+  }
+
+  return response.json()
+}
+
+
+
+
+export default async function getAiSoftwarePosts() {
+    const data = await fetchAPI('/posts/ai-software/',{next:{revalidate:10}})
+    return data
 }
