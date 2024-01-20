965f9869f06592b7b42ce81235415c60b2a7c886
shuri <shurikikuti@icloud.com>
2024-01-20 15:23:37 +1100

    restore to original state about api

--- lib/getAiSoftwarePosts.tsx
+++ lib/getAiSoftwarePosts.tsx
@@ -1,25 +1,9 @@
 import React from 'react'
 
-const fetchAPI = async (url: string, options: RequestInit) => {
-  const apiUrl = process.env.API_URL
-
-  if (!apiUrl) {
-    throw new Error("API URL not found")
-  }
-
-  const response = await fetch(`${apiUrl}${url}`, options)
-
-  if (!response.ok) {
-    throw new Error("API Error")
-  }
-
-  return response.json()
-}
-
 export default async function getAiSoftwarePosts() {
-  const data = fetchAPI("/posts/ai-software/", {
-    next: { revalidate: 10 },
-  });
+    const data = await fetch('https://nomadvision-e83b637ecb0f.herokuapp.com/posts/ai-software/', 
+      { next: { revalidate: 10 } })
+    if (!data.ok) return undefined
+  return data.json()
 
-  return data;
 }
